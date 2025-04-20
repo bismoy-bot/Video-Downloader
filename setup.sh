f3fc19ef0d67f1f07dff0c2252b4d42b1b89fcab
Bismoy Ghosh <207135760+bismoy-bot@users.noreply.github.com>
2025-04-20 12:00:10 +0600

    Update setup.sh
    Signed-off-by: Bismoy Ghosh <207135760+bismoy-bot@users.noreply.github.com>

--- setup.sh
+++ setup.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
 # Do not modify without author permission.
-
-echo "[+] Detecting operating system..."
+# Regular Colors
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+blue='\033[0;34m'
+purple='\033[0;35m'
+cyan='\033[0;36m'
+white='\033[0;37m'
+reset='\033[0m'
+echo -e "${yellow}[+] Detecting operating system...${reset}"
 
 unameOut="$(uname -s)"
 
 case "${unameOut}" in
     Linux*)
-        echo "[+] Linux detected. Running 'l'..."
+        echo -e "${purple}[+] Linux detected. Running 'linux.sh'...${reset}"
         chmod +x ./linux.sh && ./linux.sh
         ;;
     Darwin*)
-        echo "[+] macOS detected. Running 'Mac.sh'..."
+        echo -e "${purple}[+] macOS detected. Running 'Mac.sh'...${reset}"
         chmod +x ./Mac.sh && ./Mac.sh
         ;;
     *)
-        echo "[-] Unsupported OS: ${unameOut}. Only Linux and macOS are supported."
+        echo -e "${red}[-] Unsupported OS: ${unameOut}. You don't need to worry about it ...${reset}"
         exit 1
         ;;
 esac
